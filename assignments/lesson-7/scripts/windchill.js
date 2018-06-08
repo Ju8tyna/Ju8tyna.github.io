6920273523d206c01301a264340235bf64673326
Ju8tyna <35012893+Ju8tyna@users.noreply.github.com>
2018-06-08 07:15:21 -0700

    added math.round() to windchill

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -4,6 +4,6 @@ var wspeed = 5;
 
 var av = (high + low) / 2;
 
-var wchill = 35.74 + 0.6215 * av - 35.75 * (Math.pow(wspeed, 0.16)) + 0.4275 * av * (Math.pow(wspeed, 0.16));
+var wchill = Math.round(35.74 + 0.6215 * av - 35.75 * (Math.pow(wspeed, 0.16)) + 0.4275 * av * (Math.pow(wspeed, 0.16)));
 
 document.getElementById("windchill").innerHTML = wchill + "&deg;";
